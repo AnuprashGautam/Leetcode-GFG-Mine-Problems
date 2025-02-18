461042eb5bbc2668a5a141b09cc3388fee9a256a
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-18 20:31:16 +0530

    Using just one line of code  a=(a+b)-(b=a).

---
+++ Mine/0001 Two Number Swapping/Logic5- Using just one line of code/Logic4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Logic4 {
+    static void swap(int a,int b)
+    {
+        System.out.println("Before swapping:"+a+" "+b);
+
+        a=(a+b)-(b=a);
+
+        System.out.println("After swapping:"+a+" "+b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Enter the first number:");
+        int a=scanner.nextInt();
+
+        System.out.print("Enter the second number:");
+        int b=scanner.nextInt();
+
+        swap(a, b);
+    }
+}

c63fe4632916574bc39278b8aeb5c296d86d58ab
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-26 02:09:17 +0530

    Time complexity: O(logN) and Space Complexity: O(1)

---
+++ Leetcode/Problem 0050🟡 Pow (x,n)/Solution2.java
@@ -0,0 +1,25 @@
+class Solution2 {
+    public double myPow(double x, int n) {
+        double ans= 1.0;
+        long nn= n;             // Storing the power or exponent.
+
+        if (nn<0) 
+            nn = -1 * nn;
+
+        while(nn>0)
+        {
+            if( nn%2== 1)
+            {
+                ans = ans * x;
+                nn = nn - 1;
+            }else{
+                x = x * x;
+                nn = nn/2;
+            }
+        }
+
+        if(n<0)
+            ans = (double)1.0/(double)ans;
+        return ans;
+    }
+}

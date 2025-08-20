485a7f3ce9ed33f850181add9d642715855dfb7d
Anuprash <anuprashgautam@hotmail.com>
2025-08-21 02:45:25 +0530

    Time complexity: O(N) and Space Complexity: O(1)

---
+++ Leetcode/Problem 0443🟡 String Compression/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int compress(char[] chars) {
+        int read=0, write=0;
+
+        while(read < chars.length)
+        {
+            char currChar=chars[read];
+            int count=0;
+
+            // Finding the occurrences
+            while(read < chars.length && currChar==chars[read])
+            {
+                read++;
+                count++;
+            }
+
+            chars[write++]=currChar;
+
+            if(count>1)
+            {
+                for(char c: Integer.toString(count).toCharArray())
+                {
+                    chars[write++]=c;
+                }
+            }
+        }
+
+        return write;
+    }
+}
+
+// Time Complexity:- O(N)
+// Space Complexity:- O(N)
